2509cb667b4a05f8bf9f7633ea18d7a928d4c3e2
TBD <tbd_fr@yahoo.fr>
2013-05-11 00:08:59 +0300

    inject enableObservation

--- lib/motion-sparkinspector.rb
+++ lib/motion-sparkinspector.rb
@@ -6,10 +6,30 @@ unless File.exist? '/Applications/Spark Inspector.app'
 	raise "Please install SparkInspector (http://www.sparkinspector.com"
 end
 
+def create_launcher(files)
+	# --- based on motion-testflight
+	launcher_code = <<EOF
+	# created by motion-sparkinspector
+	if Object.const_defined?('SparkInspector')
+		SparkInspector.enableObservation
+	end
+EOF
+
+    launcher_file = './app/sparkinspector_launcher.rb'
+    if !File.exist?(launcher_file) or File.read(launcher_file) != launcher_code
+      File.open(launcher_file, 'w') { |io| io.write(launcher_code) }
+    end
+    files = files.flatten
+    files << launcher_file unless files.find { |x| File.expand_path(x) == File.expand_path(launcher_file) }
+
+end
+
 Motion::Project::App.setup do |app|
 	app.development do
   	app.vendor_project('/Applications/Spark Inspector.app/Contents/Resources/Frameworks/SparkInspector.framework', :static, :products => ['SparkInspector'], :force_load => true, :headers_dir => 'Headers')
 		app.libs += ['/usr/lib/libz.dylib']
 		app.frameworks += ['QuartzCore']
+
+		create_launcher(app.files)
 	end
 end
