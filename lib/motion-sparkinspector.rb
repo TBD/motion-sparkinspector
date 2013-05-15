611018b3d538060d3838a2e0462ee0500e08a9af
TBD <tbd_fr@yahoo.fr>
2013-05-15 23:04:40 +0300

    only warning if no SparkInspector. fixes #1

--- lib/motion-sparkinspector.rb
+++ lib/motion-sparkinspector.rb
@@ -1,35 +1,35 @@
 unless defined?(Motion::Project::Config)
-  raise "This file must be required within a RubyMotion project Rakefile."
-end
-
-unless File.exist? '/Applications/Spark Inspector.app'
-	raise "Please install SparkInspector (http://www.sparkinspector.com"
+	raise "This file must be required within a RubyMotion project Rakefile."
 end
 
 def create_launcher(files)
 	# --- based on motion-testflight
 	launcher_code = <<EOF
-	# created by motion-sparkinspector
+# created by motion-sparkinspector
 	if Object.const_defined?('SparkInspector')
 		SparkInspector.enableObservation
 	end
 EOF
 
-    launcher_file = './app/sparkinspector_launcher.rb'
-    if !File.exist?(launcher_file) or File.read(launcher_file) != launcher_code
-      File.open(launcher_file, 'w') { |io| io.write(launcher_code) }
-    end
-    files = files.flatten
-    files << launcher_file unless files.find { |x| File.expand_path(x) == File.expand_path(launcher_file) }
+	launcher_file = './app/sparkinspector_launcher.rb'
+	if !File.exist?(launcher_file) or File.read(launcher_file) != launcher_code
+		File.open(launcher_file, 'w') { |io| io.write(launcher_code) }
+	end
+	files = files.flatten
+	files << launcher_file unless files.find { |x| File.expand_path(x) == File.expand_path(launcher_file) }
 
 end
 
 Motion::Project::App.setup do |app|
 	app.development do
-  	app.vendor_project('/Applications/Spark Inspector.app/Contents/Resources/Frameworks/SparkInspector.framework', :static, :products => ['SparkInspector'], :force_load => true, :headers_dir => 'Headers')
-		app.libs += ['/usr/lib/libz.dylib']
-		app.frameworks += ['QuartzCore']
+		if File.exist? '/Applications/Spark Inspector.appx'
+			app.vendor_project('/Applications/Spark Inspector.app/Contents/Resources/Frameworks/SparkInspector.framework', :static, :products => ['SparkInspector'], :force_load => true, :headers_dir => 'Headers')
+			app.libs += ['/usr/lib/libz.dylib']
+			app.frameworks += ['QuartzCore']
 
-		create_launcher(app.files)
+			create_launcher(app.files)
+		else
+			puts "[warning] missing SparkInspector (http://www.sparkinspector.com)"
+		end
 	end
 end
